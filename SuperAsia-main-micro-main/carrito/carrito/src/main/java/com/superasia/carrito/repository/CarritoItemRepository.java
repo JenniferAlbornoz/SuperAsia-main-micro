24ff02ac3d4c8e10587b74f469690058b79968b0
JenniferAlbornoz <jc.albornoz.ortiz@gmail.com>
2025-06-19 02:01:00 -0400

    se corrige codigo

--- SuperAsia-main-micro-main/carrito/carrito/src/main/java/com/superasia/carrito/repository/CarritoItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.superasia.carrito.repository;
-
-import com.superasia.carrito.entity.CarritoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarritoItemRepository extends JpaRepository<CarritoItem, Long> {
-}
--- SuperAsia-main-micro-main/inventario/inventario/src/main/java/com/superasia/inventario/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.superasia.inventario.repository;
-
-import com.superasia.inventario.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
